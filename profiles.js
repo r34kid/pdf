56b352d86a98f411fbd3dc3ccee17519c9601766
Andrey <51709967+r34kid@users.noreply.github.com>
2026-02-23 22:02:39 +0300

    Обновление профилей из редактора

--- profiles.js
+++ profiles.js
@@ -1,11 +1,18 @@
 // profiles.js - общий файл с данными профилей
+// Автоматически сгенерировано редактором 23.02.2026, 22:02:38
 const profiles = [
   {
-    name: "Андрей, 55",
-    distance: "12 км от вас",
-    tags: ["188 см", "Бизнесмен", "Спорт", "Путешествия", "Авто"],
-    description: "Люблю футбол, ищу знакомство для семьи",
-    photos: [
+    "name": "Андрей, 55",
+    "distance": "12 км от вас",
+    "tags": [
+      "188 см",
+      "Бизнесмен",
+      "Спорт",
+      "Путешествия",
+      "Авто"
+    ],
+    "description": "Люблю футбол, ищу знакомство для семьи",
+    "photos": [
       "https://picsum.photos/500/900?random=1",
       "https://picsum.photos/500/900?random=2",
       "https://picsum.photos/500/900?random=3",
@@ -14,11 +21,17 @@ const profiles = [
     ]
   },
   {
-    name: "Мария, 29",
-    distance: "5 км от вас",
-    tags: ["172 см", "Маркетолог", "Йога", "Книги", "Кофе"],
-    description: "Люблю путешествия и долгие прогулки",
-    photos: [
+    "name": "Мария, 100",
+    "distance": "5 км от вас",
+    "tags": [
+      "172 см",
+      "Маркетолог",
+      "Йога",
+      "Книги",
+      "Кофе"
+    ],
+    "description": "Люблю путешествия и долгие прогулки",
+    "photos": [
       "https://picsum.photos/500/900?random=6",
       "https://picsum.photos/500/900?random=7",
       "https://picsum.photos/500/900?random=8",
@@ -27,11 +40,17 @@ const profiles = [
     ]
   },
   {
-    name: "Игорь, 34",
-    distance: "20 км от вас",
-    tags: ["180 см", "Разработчик", "Музыка", "Горы", "Сноуборд"],
-    description: "Ценю искренность и чувство юмора",
-    photos: [
+    "name": "Игорь, 34",
+    "distance": "20 км от вас",
+    "tags": [
+      "180 см",
+      "Разработчик",
+      "Музыка",
+      "Горы",
+      "Сноуборд"
+    ],
+    "description": "Ценю искренность и чувство юмора",
+    "photos": [
       "https://picsum.photos/500/900?random=11",
       "https://picsum.photos/500/900?random=12",
       "https://picsum.photos/500/900?random=13",
@@ -43,5 +62,5 @@ const profiles = [
 
 // Делаем данные доступными глобально
 if (typeof module !== 'undefined' && module.exports) {
-  module.exports = profiles; // для Node.js среды
+  module.exports = profiles;
 }
